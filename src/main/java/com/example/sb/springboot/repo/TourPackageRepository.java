65a0ea7f064f44dd9e9612707e23913c0847cc52
ud17 <upandya1297@gmail.com>
2022-10-28 19:44:13 -0400

    h2 database enabled

--- src/main/java/com/example/sb/springboot/repo/TourPackageRepository.java
+++ src/main/java/com/example/sb/springboot/repo/TourPackageRepository.java
@@ -3,6 +3,9 @@ package com.example.sb.springboot.repo;
 import com.example.sb.springboot.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 // CrudRepository interface by default provides some methods to perform queries on the database
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+    Optional<TourPackage> findByName(String name);
 }
