89e6238dec95af01df9ade4a1b12d46860a26faf
ud17 <upandya1297@gmail.com>
2022-10-29 16:48:41 -0400

    http put,patch and delete controllers

---
+++ src/main/java/com/example/sb/springboot/repo/TourRatingRepository.java
@@ -0,0 +1,17 @@
+package com.example.sb.springboot.repo;
+
+import com.example.sb.springboot.domain.TourRating;
+import com.example.sb.springboot.domain.TourRatingPk;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+import java.util.Optional;
+
+@RepositoryRestResource(exported = false)
+public interface TourRatingRepository extends CrudRepository<TourRating, TourRatingPk> {
+
+    List<TourRating> findByPkTourId(Integer tourId);
+
+    Optional<TourRating> findByPkTourIdAndPkCustomerId(Integer tourId, Integer customerId);
+}
