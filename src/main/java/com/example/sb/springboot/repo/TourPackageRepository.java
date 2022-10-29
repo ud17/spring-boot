33ef662eda12d386e59967a1eb1aed9132fe92c4
ud17 <upandya1297@gmail.com>
2022-10-29 13:17:02 -0400

    custom endpoints using @restresource

--- src/main/java/com/example/sb/springboot/repo/TourPackageRepository.java
+++ src/main/java/com/example/sb/springboot/repo/TourPackageRepository.java
@@ -2,10 +2,50 @@ package com.example.sb.springboot.repo;
 
 import com.example.sb.springboot.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
 // CrudRepository interface by default provides some methods to perform queries on the database
+// PagingAndSortingRepository - also works fine, set return type `Page` along with `Pageable` as parameter
+// example - localhost:8080/tours/search/findByTourPackageName?size=&page=&sort=title,asc
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
+// class level - defined custom endpoints
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+
     Optional<TourPackage> findByName(String name);
+
+    @Override
+    @RestResource(exported = false)
+    // method level
+    Iterable<TourPackage> findAll();
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends String> strings);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
--- src/main/java/com/example/sb/springboot/repo/TourRepository.java
+++ src/main/java/com/example/sb/springboot/repo/TourRepository.java
@@ -1,8 +1,54 @@
 package com.example.sb.springboot.repo;
 
 import com.example.sb.springboot.domain.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 // CrudRepository interface by default provides some methods to perform queries on the database
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+// PagingAndSortingRepository - also works fine, set return type `Page` along with `Pageable` as parameter
+// example - localhost:8080/tours/search/findByTourPackageName?size=&page=&sort=title,asc
+@RepositoryRestResource
+// class level - defined custom endpoints
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+
+    Page<Tour> findByTourPackageName(String name, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    Iterable<Tour> findAll(Sort sort);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
